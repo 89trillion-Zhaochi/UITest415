16a3a3f2594a2e4f9c4d26a45c9292c1c0909a68
89trillion-Zhaochi <zhaochi@89trillion.com>
2021-04-19 11:28:17 +0800

    项目目录重构
    根据代码规范重构项目目录

---
+++ Assets/Script/CountDown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public Text text;
+    [FormerlySerializedAs("TotalTime")] public int totalTime = 60;
+
+    void Start()
+    {
+        StartCoroutine(Time());
+    }
+
+    IEnumerator Time()
+    {
+        while (totalTime >= 0)
+        {
+            text.text = "Refersh time:" + totalTime.ToString();
+            yield return new WaitForSeconds(1);
+            totalTime--;
+        }
+
+        if (totalTime == 0)
+        {
+            totalTime = 10;
+        }
+    }
+
+    void update()
+    {
+        if (totalTime == 0)
+        {
+        }
+    }
+}
