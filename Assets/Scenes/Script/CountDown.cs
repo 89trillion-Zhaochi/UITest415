82185b70857137c7b434c4f275accf4e3ea0479e
89trillion-Zhaochi <zhaochi@89trillion.com>
2021-04-19 10:45:16 +0800

    Initial commit

---
+++ Assets/Scenes/Script/CountDown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public Text text;
+    public int TotalTime = 60;
+
+    void Start()
+    {
+        StartCoroutine(Time());
+    }
+
+    IEnumerator Time()
+    {
+        while (TotalTime >= 0)
+        {
+            text.text = "Refersh time:" + TotalTime.ToString();
+            yield return new WaitForSeconds(1);
+            TotalTime--;
+        }
+
+        if (TotalTime == 0)
+        {
+            TotalTime = 10;
+        }
+    }
+
+    void update()
+    {
+        if (TotalTime == 0)
+        {
+        }
+    }
+}
---
+++ Assets/Scenes/Script/IsPurchased.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class IsPurchased : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenDailySelection()
+    {
+        GetComponent<CanvasGroup>().alpha = 1;
+        GetComponent<CanvasGroup>().interactable = true;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        
+    }
+    public void CloseDailySelection()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+        GetComponent<CanvasGroup>().interactable = false;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        text.text = "Collected!";
+    }
+}
